535996f141265e2c146c94eec248f4598855432d
rafael-graunke <rafael.graunke@yahoo.com>
2023-08-22 00:10:44 -0300

    feat: added example rom and rom reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_rom(path: &String) -> Vec<u8> {
+    let mut file = File::open(&path).unwrap();
+    let mut data = Vec::new();
+    file.read_to_end(&mut data).unwrap();
+
+    data
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let rom_path = &args[1];
+
+    let rom = read_rom(rom_path);
+
+    println!("{rom:?}");
 }
