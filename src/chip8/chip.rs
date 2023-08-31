b1c562294a0637930bf59e50d287090b2aeaf866
rafael-graunke <rafael.graunke@yahoo.com>
2023-08-30 22:36:02 -0300

    fix: handler behaviour

--- src/chip8/chip.rs
+++ src/chip8/chip.rs
@@ -1,28 +1,23 @@
-use rand::Rng;
 use sdl2::audio::{AudioDevice, AudioSpecDesired};
 use sdl2::event::Event;
-use sdl2::keyboard::{Keycode, Scancode};
+use sdl2::keyboard::Keycode;
 use sdl2::EventPump;
-use std::collections::HashMap;
-use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 
 use crate::audio::SquareWave;
-use crate::screen::{Screen, SCREEN_HEIGHT, SCREEN_WIDTH};
+use crate::screen::Screen;
 
-use crate::chip8::opcode::OpCode;
+use crate::chip8::opcodes::{handlers, parser};
 use crate::chip8::quirks::Quirks;
 use crate::chip8::state::ChipState;
 
 const MEM_OFFSET: u16 = 0x200;
-const FONT_OFFSET: u16 = 0x50;
-const DEBUG: bool = true;
+const DEBUG: bool = false;
 
 pub struct Chip8 {
     state: ChipState,
     display: Screen,
-    opcode: OpCode,
     sound_device: AudioDevice<SquareWave>,
     event_pump: EventPump,
     quirks: Quirks,
@@ -54,7 +49,6 @@ impl Chip8 {
         Chip8 {
             state: ChipState::init(),
             display: Screen::new(&sdl_context),
-            opcode: OpCode::init(),
             sound_device: device,
             event_pump: event_pump,
             quirks: Quirks::for_chip8(),
@@ -83,56 +77,11 @@ impl Chip8 {
         self.state.running
     }
 
-    fn fetch_opcode(&mut self) {
-        let first_byte = self.state.memory[self.state.pc as usize] as u16;
-        let second_byte = self.state.memory[(self.state.pc + 1) as usize] as u16;
+    pub fn fetch(&self) -> u16 {
+        let first_byte = self.state.memory[self.state.pc as usize];
+        let second_byte = self.state.memory[self.state.pc as usize + 1];
 
-        let new_code = (first_byte << 8) | second_byte;
-
-        self.opcode.set(new_code);
-    }
-
-    fn run_opcode(&mut self) {
-        // Check single nibble determinant opcodes
-        match self.opcode.mask(0xF000) {
-            0x1000 => self.jump(),
-            0x2000 => self.call_subroutine(),
-            0x3000 => self.skip_equal(),
-            0x4000 => self.skip_not_equal(),
-            0x5000 => self.skip_register_equal(),
-            0x6000 => self.load_register(),
-            0x7000 => self.add_to_register(),
-            0x8000 => self.logic_op(),
-            0x9000 => self.skip_register_not_equal(),
-            0xA000 => self.set_vi(),
-            0xB000 => self.jump_with_offset(),
-            0xC000 => self.random_number(),
-            0xD000 => self.draw(),
-            _ => {}
-        }
-
-        // Check dual nibble determinant opcodes
-        match self.opcode.mask(0xF00F) {
-            0x0000 => self.clear_screen(),
-            0x000E => self.return_subroutine(),
-            0xE00E => self.skip_if_key(),
-            0xE001 => self.skip_if_not_key(),
-            _ => {}
-        }
-
-        // Check F opcodes
-        match self.opcode.mask(0xF0FF) {
-            0xF007 => self.load_from_dt(),
-            0xF00A => self.wait_for_input(),
-            0xF015 => self.load_to_dt(),
-            0xF018 => self.load_to_st(),
-            0xF01E => self.add_to_index(),
-            0xF029 => self.set_font_character(),
-            0xF033 => self.binary_coded_decimal(),
-            0xF055 => self.load_to_memory(),
-            0xF065 => self.load_from_memory(),
-            _ => {}
-        }
+        ((first_byte as u16) << 8) | second_byte as u16
     }
 
     pub fn step(&mut self, ipf: u32) {
@@ -153,8 +102,15 @@ impl Chip8 {
 
         // Run N instructions per seconds
         for _ in 0..ipf {
-            self.fetch_opcode();
-            self.run_opcode();
+            let code = self.fetch();
+
+            handlers::run(
+                code,
+                &mut self.state,
+                &self.quirks,
+                &mut self.display,
+                &mut self.event_pump,
+            );
 
             if !self.state.did_jump && !self.state.should_wait {
                 self.state.pc += 2;
@@ -182,373 +138,5 @@ impl Chip8 {
         if DEBUG {
             println!("{:?}", self.state);
         };
-    }
-
-    fn jump_with_offset(&mut self) {
-        let mut vx = self.opcode.get_x();
-
-        if self.quirks.has_jumping() {
-            vx = 0;
-        }
-
-        let address = self.opcode.get_3n() + self.state.registers[vx as usize] as u16;
-
-        self.state.pc = address;
-        self.state.did_jump = true;
-    }
-
-    fn add_to_index(&mut self) {
-        let vx = self.opcode.get_x();
-        self.state.vi += self.state.registers[vx as usize] as u16;
-    }
-
-    fn key_map(key: u8) -> Scancode {
-        let key_mapping = HashMap::from([
-            (0x1, Scancode::Num1),
-            (0x2, Scancode::Num2),
-            (0x3, Scancode::Num3),
-            (0xC, Scancode::Num4),
-            (0x4, Scancode::Q),
-            (0x5, Scancode::W),
-            (0x6, Scancode::E),
-            (0xD, Scancode::R),
-            (0x7, Scancode::A),
-            (0x8, Scancode::S),
-            (0x9, Scancode::D),
-            (0xE, Scancode::F),
-            (0xA, Scancode::Z),
-            (0x0, Scancode::X),
-            (0xB, Scancode::C),
-            (0xF, Scancode::V),
-        ]);
-
-        *key_mapping.get(&key).unwrap()
-    }
-
-    fn skip_if_key(&mut self) {
-        let vx = self.opcode.get_x();
-
-        let key = Chip8::key_map(self.state.registers[vx as usize]);
-
-        if self
-            .event_pump
-            .keyboard_state()
-            .pressed_scancodes()
-            .any(|x| x == key)
-        {
-            self.state.pc += 2;
-        };
-    }
-
-    fn skip_if_not_key(&mut self) {
-        let vx = self.opcode.get_x();
-
-        let key = Chip8::key_map(self.state.registers[vx as usize]);
-
-        if self
-            .event_pump
-            .keyboard_state()
-            .pressed_scancodes()
-            .all(|x| x != key)
-        {
-            self.state.pc += 2;
-        };
-    }
-
-    fn wait_for_input(&mut self) {
-        self.state.should_wait = true;
-        let vx = self.opcode.get_x();
-
-        let key_mapping = HashMap::from([
-            (Keycode::Num1, 0x1),
-            (Keycode::Num2, 0x2),
-            (Keycode::Num3, 0x3),
-            (Keycode::Num4, 0xC),
-            (Keycode::Q, 0x4),
-            (Keycode::W, 0x5),
-            (Keycode::E, 0x6),
-            (Keycode::R, 0xD),
-            (Keycode::A, 0x7),
-            (Keycode::S, 0x8),
-            (Keycode::D, 0x9),
-            (Keycode::F, 0xE),
-            (Keycode::Z, 0xA),
-            (Keycode::X, 0x0),
-            (Keycode::C, 0xB),
-            (Keycode::V, 0xF),
-        ]);
-
-        for event in self.event_pump.poll_iter() {
-            match event {
-                Event::KeyUp {
-                    keycode: Some(keycode),
-                    ..
-                } => match key_mapping.get(&keycode) {
-                    Some(key) => {
-                        self.state.registers[vx as usize] = *key;
-                        self.state.should_wait = false;
-                    }
-                    _ => {}
-                },
-                _ => {}
-            }
-        }
-    }
-
-    fn load_from_dt(&mut self) {
-        let vx = self.opcode.get_x();
-        self.state.registers[vx as usize] = self.state.delay_timer;
-    }
-
-    fn load_to_dt(&mut self) {
-        let vx = self.opcode.get_x();
-        self.state.delay_timer = self.state.registers[vx as usize];
-    }
-
-    fn load_to_st(&mut self) {
-        let vx = self.opcode.get_x();
-        self.state.sound_timer = self.state.registers[vx as usize];
-    }
-
-    fn set_font_character(&mut self) {
-        let vx = self.opcode.get_x();
-        let x = self.state.registers[vx as usize];
-        self.state.vi = FONT_OFFSET + (x * 5) as u16;
-    }
-
-    fn load_to_memory(&mut self) {
-        let x = self.opcode.get_x() as u16;
-
-        for i in 0..=x {
-            let mem_address = self.state.vi + i;
-            self.state.memory[mem_address as usize] = self.state.registers[i as usize];
-        }
-
-        if self.quirks.has_increment_index() {
-            self.state.vi += x + 1;
-        }
-    }
-
-    fn load_from_memory(&mut self) {
-        let x = self.opcode.get_x() as u16;
-
-        for i in 0..=x {
-            let mem_address = (self.state.vi) + i;
-            self.state.registers[i as usize] = self.state.memory[mem_address as usize];
-        }
-    }
-
-    fn binary_coded_decimal(&mut self) {
-        let vx = self.opcode.get_x();
-        let x = self.state.registers[vx as usize];
-
-        let address = self.state.vi;
-
-        self.state.memory[(address) as usize] = x / 100;
-        self.state.memory[(address + 1) as usize] = (x % 100) / 10;
-        self.state.memory[(address + 2) as usize] = x % 10;
-    }
-
-    fn logic_op(&mut self) {
-        let vx = self.opcode.get_x() as usize;
-        let x = self.state.registers[vx];
-
-        let vy = self.opcode.get_y();
-        let y = self.state.registers[vy as usize];
-
-        let operation = self.opcode.get_n();
-
-        self.state.registers[0xF] = 0;
-        self.state.registers[vx] = match operation {
-            0 => y,
-            1 => x | y,
-            2 => x & y,
-            3 => x ^ y,
-            4 => self.sum_overflow(x, y),
-            5 => self.subtract_overflow(x, y),
-            6 => {
-                self.state.registers[0xF] = x & 1;
-                if self.quirks.has_shifting() {
-                    y >> 1
-                } else {
-                    x >> 1
-                }
-            }
-            7 => self.subtract_overflow(y, x),
-            0xE => {
-                self.state.registers[0xF] = (x & 0x80) >> 7;
-                if self.quirks.has_shifting() {
-                    y << 1
-                } else {
-                    x << 1
-                }
-            }
-            _ => 0,
-        }
-    }
-
-    fn sum_overflow(&mut self, n1: u8, n2: u8) -> u8 {
-        let n1 = n1 as u16;
-        let n2 = n2 as u16;
-
-        let sum = n1 + n2;
-
-        if sum > 255 {
-            self.state.registers[0xF] = 1;
-        } else {
-            self.state.registers[0xF] = 0;
-        }
-
-        return sum as u8;
-    }
-
-    fn subtract_overflow(&mut self, n1: u8, n2: u8) -> u8 {
-        let mut n1 = n1 as u16;
-        let n2 = n2 as u16;
-
-        self.state.registers[0xF] = 1;
-
-        if n1 < n2 {
-            self.state.registers[0xF] = 0;
-            n1 |= 0x100;
-        }
-
-        (n1 - n2) as u8
-    }
-
-    fn random_number(&mut self) {
-        let mut rng = rand::thread_rng();
-
-        let vx = self.opcode.get_x() as usize;
-
-        let nn: u8 = self.opcode.get_2n() as u8;
-        let random: u8 = rng.gen();
-
-        self.state.registers[vx] = random & nn;
-    }
-
-    fn skip_equal(&mut self) {
-        let vx = self.opcode.get_x() as usize;
-        let x = self.state.registers[vx];
-
-        let value = self.opcode.get_2n();
-        if x == value {
-            self.state.pc += 2;
-        }
-    }
-
-    fn skip_not_equal(&mut self) {
-        let vx = self.opcode.get_x() as usize;
-        let x = self.state.registers[vx];
-
-        let value = self.opcode.get_2n();
-        if x != value {
-            self.state.pc += 2;
-        }
-    }
-
-    fn skip_register_equal(&mut self) {
-        let vx = self.opcode.get_x() as usize;
-        let x = self.state.registers[vx];
-
-        let vy = self.opcode.get_y() as usize;
-        let y = self.state.registers[vy];
-
-        if x == y {
-            self.state.pc += 2;
-        }
-    }
-
-    fn skip_register_not_equal(&mut self) {
-        let vx = self.opcode.get_x() as usize;
-        let x = self.state.registers[vx];
-
-        let vy = self.opcode.get_y() as usize;
-        let y = self.state.registers[vy];
-
-        if x != y {
-            self.state.pc += 2;
-        }
-    }
-
-    fn call_subroutine(&mut self) {
-        self.state.stack.push(self.state.pc);
-        self.state.pc = self.opcode.get_3n();
-        self.state.did_jump = true;
-    }
-
-    fn return_subroutine(&mut self) {
-        self.state.pc = self.state.stack.pop().unwrap();
-    }
-
-    fn clear_screen(&mut self) {
-        self.display.clear();
-    }
-
-    fn jump(&mut self) {
-        self.state.pc = self.opcode.get_3n();
-        self.state.did_jump = true;
-    }
-
-    fn load_register(&mut self) {
-        let index = self.opcode.get_x();
-        let value = self.opcode.get_2n();
-        self.state.registers[index as usize] = value as u8;
-    }
-
-    fn add_to_register(&mut self) {
-        let index = self.opcode.get_x();
-        let value = self.opcode.get_2n();
-
-        // This overflow does not affect F flag
-        let mut sum = value as u16 + self.state.registers[index as usize] as u16;
-
-        if sum > 255 {
-            sum -= 256;
-        }
-
-        self.state.registers[index as usize] = sum as u8;
-    }
-
-    fn set_vi(&mut self) {
-        self.state.vi = self.opcode.get_3n();
-    }
-
-    fn draw(&mut self) {
-        self.state.should_draw = true;
-
-        let vx = self.opcode.get_x() as usize;
-        let vy = self.opcode.get_y() as usize;
-
-        let x = self.state.registers[vx] & (SCREEN_WIDTH - 1);
-        let y = (self.state.registers[vy] & (SCREEN_HEIGHT as u8 - 1)) as usize;
-
-        let n = self.opcode.get_n() as usize;
-
-        self.state.registers[0xF] = 0;
-
-        for index in 0..n {
-            let line =
-                &mut self.display.screen_memory[(y + index) & (SCREEN_HEIGHT as u8 - 1) as usize];
-
-            let address = self.state.vi + index as u16;
-
-            let sprite = self.state.memory[address as usize] as u64;
-
-            let offset_sprite = sprite << (SCREEN_WIDTH - 8) >> x;
-
-            let new_line = *line ^ offset_sprite;
-
-            for bit in 0..SCREEN_WIDTH {
-                let bit_before = *line & (0x8000000000000000 >> bit);
-                let bit_after = new_line & (0x8000000000000000 >> bit);
-
-                if (bit_before != bit_after) && (bit_before >> (SCREEN_WIDTH - 1 - bit)) == 1 {
-                    self.state.registers[0xF] = 1;
-                }
-            }
-
-            *line = new_line;
-        }
     }
 }
--- src/chip8/opcodes/handlers.rs
+++ src/chip8/opcodes/handlers.rs
@@ -5,15 +5,16 @@ use crate::chip8::quirks::Quirks;
 use crate::chip8::state::ChipState;
 use crate::screen::{Screen, SCREEN_HEIGHT, SCREEN_WIDTH};
 use rand::Rng;
+use sdl2::event::Event;
+use sdl2::keyboard::{Keycode, Scancode};
 use sdl2::EventPump;
-use sdl2::keyboard::Scancode;
 
 pub fn run(
     opcode: u16,
     state: &mut ChipState,
-    quirks: Quirks,
+    quirks: &Quirks,
     screen: &mut Screen,
-    events: &EventPump,
+    events: &mut EventPump,
 ) {
     match parser::get_digit(opcode) {
         0x0 => run_00en(opcode, state, screen),
@@ -27,16 +28,16 @@ pub fn run(
         0x8 => run_8xyn(opcode, state, quirks.has_shifting()),
         0x9 => run_9xy0(opcode, state),
         0xA => run_annn(opcode, state),
-        0xB => run_bnnn(opcode, state, quirks.has_jumping()),
+        0xB => run_bnnn(opcode, state, !quirks.has_jumping()),
         0xC => run_cxnn(opcode, state),
         0xD => run_dxyn(opcode, state, screen),
         0xE => run_exnn(opcode, state, events),
-        0xF => (),
+        0xF => run_fxnn(opcode, state, events),
         _ => {}
     }
 }
 
-fn run_00en(opcode: u16, state: &mut ChipState, screen: &mut Screen) {
+pub fn run_00en(opcode: u16, state: &mut ChipState, screen: &mut Screen) {
     match parser::get_n(opcode) {
         0x0 => screen.clear(),
         0xE => state.pc = state.stack.pop().unwrap(),
@@ -44,62 +45,62 @@ fn run_00en(opcode: u16, state: &mut ChipState, screen: &mut Screen) {
     }
 }
 
-fn run_1nnn(opcode: u16, state: &mut ChipState) {
+pub fn run_1nnn(opcode: u16, state: &mut ChipState) {
     state.pc = parser::get_3n(opcode);
     state.did_jump = true;
 }
 
-fn run_2nnn(opcode: u16, state: &mut ChipState) {
+pub fn run_2nnn(opcode: u16, state: &mut ChipState) {
     state.stack.push(state.pc);
     state.pc = parser::get_3n(opcode);
     state.did_jump = true;
 }
 
-fn run_3xnn(opcode: u16, state: &mut ChipState) {
+pub fn run_3xnn(opcode: u16, state: &mut ChipState) {
     let x = parser::get_x(opcode) as usize;
-
-    let vx = state.memory[x];
+    let vx = state.registers[x];
 
     if vx == parser::get_2n(opcode) {
         state.skip();
     }
 }
 
-fn run_4xnn(opcode: u16, state: &mut ChipState) {
+pub fn run_4xnn(opcode: u16, state: &mut ChipState) {
     let x = parser::get_x(opcode) as usize;
-
-    let vx = state.memory[x];
+    let vx = state.registers[x];
 
     if vx != parser::get_2n(opcode) {
         state.skip();
     }
 }
 
-fn run_5xy0(opcode: u16, state: &mut ChipState) {
+pub fn run_5xy0(opcode: u16, state: &mut ChipState) {
     let x = parser::get_x(opcode) as usize;
     let vx = state.registers[x];
 
     let y = parser::get_y(opcode) as usize;
     let vy = state.registers[y];
 
-    if x == y {
+    if vx == vy {
         state.skip();
     }
 }
 
-fn run_6xnn(opcode: u16, state: &mut ChipState) {
+pub fn run_6xnn(opcode: u16, state: &mut ChipState) {
     let x = parser::get_x(opcode) as usize;
 
     state.registers[x] = parser::get_2n(opcode);
 }
 
-fn run_7xnn(opcode: u16, state: &mut ChipState) {
+pub fn run_7xnn(opcode: u16, state: &mut ChipState) {
     let x = parser::get_x(opcode) as usize;
 
-    state.registers[x] += parser::get_2n(opcode);
+    let sum = state.registers[x] as u16 + parser::get_2n(opcode) as u16;
+
+    state.registers[x] = sum as u8;
 }
 
-fn run_8xyn(opcode: u16, state: &mut ChipState, change_x: bool) {
+pub fn run_8xyn(opcode: u16, state: &mut ChipState, change_x: bool) {
     let x = parser::get_x(opcode) as usize;
     let y = parser::get_y(opcode) as usize;
     let op = parser::get_n(opcode);
@@ -131,10 +132,11 @@ fn run_8xyn(opcode: u16, state: &mut ChipState, change_x: bool) {
             }
         }
         0x6 => {
-            state.registers[15] = vx & 1;
             if change_x {
+                state.registers[15] = vy & 1;
                 vy >> 1
             } else {
+                state.registers[15] = vx & 1;
                 vx >> 1
             }
         }
@@ -148,33 +150,35 @@ fn run_8xyn(opcode: u16, state: &mut ChipState, change_x: bool) {
             }
         }
         0xE => {
-            state.registers[15] = (vx & 0x80) >> 7;
             if change_x {
-                vy >> 1
+                state.registers[15] = (vy & 0x80) >> 7;
+                vy << 1
             } else {
-                vx >> 1
+                state.registers[15] = (vx & 0x80) >> 7;
+                vx << 1
             }
         }
+        _ => 0,
     }
 }
 
-fn run_9xy0(opcode: u16, state: &mut ChipState) {
+pub fn run_9xy0(opcode: u16, state: &mut ChipState) {
     let x = parser::get_x(opcode) as usize;
     let vx = state.registers[x];
 
     let y = parser::get_y(opcode) as usize;
     let vy = state.registers[y];
 
-    if x != y {
+    if vx != vy {
         state.skip();
     }
 }
 
-fn run_annn(opcode: u16, state: &mut ChipState) {
+pub fn run_annn(opcode: u16, state: &mut ChipState) {
     state.vi = parser::get_3n(opcode);
 }
 
-fn run_bnnn(opcode: u16, state: &mut ChipState, use_x: bool) {
+pub fn run_bnnn(opcode: u16, state: &mut ChipState, use_x: bool) {
     let x = if use_x { parser::get_x(opcode) } else { 0 };
 
     let address = parser::get_3n(opcode) + state.registers[x as usize] as u16;
@@ -183,7 +187,7 @@ fn run_bnnn(opcode: u16, state: &mut ChipState, use_x: bool) {
     state.did_jump = true;
 }
 
-fn run_cxnn(opcode: u16, state: &mut ChipState) {
+pub fn run_cxnn(opcode: u16, state: &mut ChipState) {
     let mut rng = rand::thread_rng();
 
     let x = parser::get_x(opcode) as usize;
@@ -193,24 +197,28 @@ fn run_cxnn(opcode: u16, state: &mut ChipState) {
     state.registers[x] = random & parser::get_2n(opcode);
 }
 
-fn run_dxyn(opcode: u16, state: &mut ChipState, screen: &mut Screen) {
+pub fn run_dxyn(opcode: u16, state: &mut ChipState, screen: &mut Screen) {
     state.should_draw = true;
 
     let x = parser::get_x(opcode) as usize;
+    let vx = state.registers[x] & (SCREEN_WIDTH - 1);
+
     let y = parser::get_y(opcode) as usize;
-    let n = parser::get_n(opcode) as usize;
+    let vy = state.registers[y] & (SCREEN_HEIGHT as u8 - 1);
+
+    let n = parser::get_n(opcode);
 
     state.registers[15] = 0;
 
     for index in 0..n {
-        let wrapped_position = (y + index) & (SCREEN_HEIGHT - 1);
-        let line = &mut screen.screen_memory[wrapped_position];
+        let wrap_pos = (vy + index) & (SCREEN_HEIGHT - 1) as u8;
+        let line = &mut screen.screen_memory[wrap_pos as usize];
 
         let address = state.vi + index as u16;
 
         let sprite = state.memory[address as usize] as u64;
 
-        let offset_sprite = sprite << (SCREEN_WIDTH - 8) >> x;
+        let offset_sprite = sprite << (SCREEN_WIDTH - 8) >> vx;
 
         let new_line = *line ^ offset_sprite;
 
@@ -219,7 +227,7 @@ fn run_dxyn(opcode: u16, state: &mut ChipState, screen: &mut Screen) {
             let bit_after = new_line & (0x8000000000000000 >> bit);
 
             if (bit_before != bit_after) && (bit_before >> (SCREEN_WIDTH - 1 - bit)) == 1 {
-                state.registers[0xF] = 1;
+                state.registers[15] = 1;
             }
         }
 
@@ -227,21 +235,102 @@ fn run_dxyn(opcode: u16, state: &mut ChipState, screen: &mut Screen) {
     }
 }
 
-fn run_exnn(opcode: u16, state: &mut ChipState, events: &EventPump) {
+pub fn run_exnn(opcode: u16, state: &mut ChipState, events: &mut EventPump) {
     let x = parser::get_x(opcode) as usize;
     let vx = state.registers[x];
 
-    let key = key_map(vx);
+    let key = u8_to_scancode(vx);
 
     match parser::get_2n(opcode) {
-        0x9E => {},
-        0xA1 => {},
+        0x9E => {
+            if events
+                .keyboard_state()
+                .pressed_scancodes()
+                .any(|code| code == key)
+            {
+                state.skip()
+            }
+        }
+        0xA1 => {
+            if events
+                .keyboard_state()
+                .pressed_scancodes()
+                .all(|code| code != key)
+            {
+                state.skip()
+            }
+        }
         _ => {}
     }
-
 }
 
-fn key_map(key: u8) -> Scancode {
+pub fn run_fxnn(opcode: u16, state: &mut ChipState, events: &mut EventPump) {
+    fn wait_for_key(x: usize, state: &mut ChipState, events: &mut EventPump) {
+        state.should_wait = true;
+
+        for event in events.poll_iter() {
+            match event {
+                Event::KeyUp {
+                    keycode: Some(keycode),
+                    ..
+                } => match keycode_to_u8(keycode) {
+                    Some(key) => {
+                        state.registers[x as usize] = key;
+                        state.should_wait = false;
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
+    }
+
+    fn load_to_memory(x: u16, state: &mut ChipState, update_vi: bool) {
+        for i in 0..=x {
+            let mem_address = state.vi + i;
+            state.memory[mem_address as usize] = state.registers[i as usize];
+        }
+
+        if update_vi {
+            state.vi += x + 1;
+        }
+    }
+
+    fn load_from_memory(x: u16, state: &mut ChipState) {
+        for i in 0..=x {
+            let addr = state.vi + i;
+            state.registers[i as usize] = state.memory[addr as usize];
+        }
+    }
+
+    fn binary_coded_decimal(vx: u8, state: &mut ChipState) {
+        let address = state.vi;
+
+        state.memory[(address) as usize] = vx / 100;
+        state.memory[(address + 1) as usize] = (vx % 100) / 10;
+        state.memory[(address + 2) as usize] = vx % 10;
+    }
+
+    let x = parser::get_x(opcode) as usize;
+    let vx = &mut state.registers[x];
+
+    let op = parser::get_2n(opcode);
+
+    match op {
+        0x07 => *vx = state.delay_timer,
+        0x0A => wait_for_key(x, state, events),
+        0x15 => state.delay_timer = *vx,
+        0x18 => state.sound_timer = *vx,
+        0x1E => state.vi += *vx as u16,
+        0x29 => state.vi = 0x50 + (*vx * 5) as u16,
+        0x33 => binary_coded_decimal(*vx, state),
+        0x55 => load_to_memory(x as u16, state, true),
+        0x65 => load_from_memory(x as u16, state),
+        _ => {}
+    }
+}
+
+fn u8_to_scancode(key: u8) -> Scancode {
     let key_mapping = HashMap::from([
         (0x1, Scancode::Num1),
         (0x2, Scancode::Num2),
@@ -262,4 +351,27 @@ fn key_map(key: u8) -> Scancode {
     ]);
 
     *key_mapping.get(&key).unwrap()
+}
+
+fn keycode_to_u8(key: Keycode) -> Option<u8> {
+    let key_mapping: HashMap<Keycode, u8> = HashMap::from([
+        (Keycode::Num1, 0x1),
+        (Keycode::Num2, 0x2),
+        (Keycode::Num3, 0x3),
+        (Keycode::Num4, 0xC),
+        (Keycode::Q, 0x4),
+        (Keycode::W, 0x5),
+        (Keycode::E, 0x6),
+        (Keycode::R, 0xD),
+        (Keycode::A, 0x7),
+        (Keycode::S, 0x8),
+        (Keycode::D, 0x9),
+        (Keycode::F, 0xE),
+        (Keycode::Z, 0xA),
+        (Keycode::X, 0x0),
+        (Keycode::C, 0xB),
+        (Keycode::V, 0xF),
+    ]);
+
+    key_mapping.get(&key).copied()
 }
