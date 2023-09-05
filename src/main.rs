0afc7623ccc689db06188df80020c3b07bafb507
rafael-graunke <rafael.graunke@yahoo.com>
2023-09-05 10:17:42 -0300

    feat: starts ratatui-rs setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate sdl2;
 
 use std::env;
+use std::fmt::Write;
+use std::io::Stdout;
 use std::time::{Duration, SystemTime};
 
 use crate::core::chip::Chip8;
@@ -11,22 +13,93 @@ mod screen;
 
 const FPS: u128 = 60;
 
-fn main() -> Result<(), String> {
-    let args: Vec<String> = env::args().collect(); // check for argparser
-    let rom_path = &args[1];
-    let ipf: u32 = args[2].parse::<u32>().unwrap();
+use crossterm::event::{KeyCode, KeyEvent, Event};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::prelude::Rect;
+use ratatui::widgets::block;
+use ratatui::Frame;
+use crossterm::event::KeyCode::{Char, Esc};
+use ratatui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
+use std::{io, thread};
 
-    let mut chip = Chip8::new();
-    chip.read_rom(rom_path);
+fn main() -> Result<(), io::Error> {
+    // Init Terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    while chip.is_running() {
-        let start = SystemTime::now();
+    terminal.draw(|f| {
+        layout::<CrosstermBackend<Stdout>>(f);
+    })?;
 
-        chip.step(ipf);
+    // Start a thread to discard any input events. Without handling events, the
+    // stdin buffer will fill up, and be read into the shell when the program exits.
+    thread::spawn(|| loop {
+        println!("{:?}", event::read());
+        if let Ok(new_event) = event::read() {
+            match new_event {
+                Event::Key(KeyEvent {code: Esc, ..}) => {println!("vai sair")},
+                Event::Key(KeyEvent {code: Char('/'), ..}) => println!("click na barra"),
+                _ => {}
+            }
+        };
+    });
 
-        let sleep_for = start.elapsed().unwrap().as_nanos() + 1_000_000_000 / FPS;
-        ::std::thread::sleep(Duration::new(0, sleep_for as u32));
-    }
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
     Ok(())
+}
+
+use ratatui::{
+    backend::Backend,
+    layout::{Constraint, Direction, Layout},
+};
+
+fn layout<B: Backend>(f: &mut Frame<B>) {
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(vec![Constraint::Percentage(70), Constraint::Percentage(30)])
+        .split(f.size());
+
+    let top_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(80), Constraint::Percentage(20)])
+        .split(layout[0]);
+
+    let bottom_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(layout[1]);
+
+    let block = Block::new().borders(Borders::ALL).title("Game");
+    f.render_widget(block, top_layout[0]);
+
+    let block = Block::new().borders(Borders::ALL).title("OpCodes");
+    f.render_widget(block, top_layout[1]);
+
+    let block = Block::new().borders(Borders::ALL).title("Terminal");
+    f.render_widget(block, bottom_layout[0]);
+
+    let block = Block::new().borders(Borders::ALL).title("State");
+    f.render_widget(block, bottom_layout[1]);
 }
---
+++ src/terminal.rs
@@ -0,0 +1,23 @@
+use ratatui::{
+    backend::Backend,
+    layout::{Constraint, Direction, Layout},
+};
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+    let block = Block::default().title("Block").borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+    let block = Block::default().title("Block 2").borders(Borders::ALL);
+    f.render_widget(block, chunks[1]);
+}
